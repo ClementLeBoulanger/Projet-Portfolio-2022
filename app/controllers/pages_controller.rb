a766ae5fc5d8dc4ede3b47795edb195f85066ebd
Clement Le Boulanger <hello@clement-leboulanger.com>
2022-01-06 16:21:49 +0100

    Add bandeau GitHub

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,22 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    git_info
+  end
+
+  private
+
+  def git_info
+    require 'open-uri'
+    require 'nokogiri'
+
+    url = "https://github.com/ClementLeBoulanger"
+
+    html_file = URI.open(url).read
+    html_doc = Nokogiri::HTML(html_file)
+
+    html_doc.search('.js-yearly-contributions h2').each do |element|
+      @commit = element.text.strip.match(/[0-9]*/)
+    end
   end
 end
