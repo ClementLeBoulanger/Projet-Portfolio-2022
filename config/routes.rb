f58f6cb7fb45a3a953d7682e80079d48bc33f28d
Clement Le Boulanger <hello@clement-leboulanger.com>
2022-01-13 21:52:43 +0100

    Add Dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
     get '/' => 'messages#new'
   end
   resources :messages, only: [:create]
+  get '/admin' => 'admins#admin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
