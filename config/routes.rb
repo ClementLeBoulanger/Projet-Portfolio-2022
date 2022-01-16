aaef7e6688ee6003af92c2a09054f84d34ef4c91
Clement Le Boulanger <hello@clement-leboulanger.com>
2022-01-16 23:18:18 +0100

    Add Resources to Projet and Article

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
     get '/' => 'messages#new'
   end
   resources :messages, only: [ :create, :destroy ]
-  resources :article, only: [ :create, :destroy ]
+  resources :article
   get '/admin' => 'admins#admin' do
     get '/admin' => 'projet#new'
   end
-  resources :projets, only: [ :create, :destroy ]
+  resources :projets
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
